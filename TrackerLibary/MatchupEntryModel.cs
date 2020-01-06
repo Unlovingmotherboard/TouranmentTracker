74a9d79f21968475694e637b62d611aeb3c75d55
Unlovingmotherboard <jmhern25@yahoo.com>
2020-01-05 18:45:21 -0600

    Add project files.

---
+++ TrackerLibary/MatchupEntryModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibary
+{
+	public class MatchupEntryModel
+	{
+		/// <summary>
+		/// Represents: one team in the matchup.
+		/// </summary>
+		public TeamModel TeamCompeting { get; set; }
+
+		/// <summary>
+		/// Represents the score for this particular team
+		/// </summary>
+		public double Score { get; set; }
+
+		/// <summary>
+		/// Represents the matchup that this team came 
+		/// from the winner
+		/// </summary>
+		public MatchupModel ParentMatchup { get; set; }
+
+		/// <summary>
+		/// Can document here
+		/// </summary>
+		/// <param name="initialScore">
+		/// And document here for the parameter
+		/// </param>
+		public MatchupEntryModel(double initialScore)
+		{
+			Console.WriteLine();
+		}
+	}
+}
---
+++ TrackerLibary/PrizeModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackerLibary
+{
+	public class PrizeModel
+	{
+		public int PlaceNumber { get; set; }
+		public string PlaceName { get; set; }
+		public decimal PriceAmount { get; set; }
+		public double PrizePercentage { get; set; }
+	}
+}
