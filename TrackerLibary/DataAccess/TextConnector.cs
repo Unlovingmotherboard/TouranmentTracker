abb41ed8c60fccced8a1ea56634e14dff05972f3
Unlovingmotherboard <jmhern25@yahoo.com>
2020-01-22 16:32:43 -0600

    Fixed the issue with two class libraries. Currently wiring up the textconnector to the create prize form

--- TrackerLibary/DataAccess/TextConnector.cs
+++ TrackerLibary/DataAccess/TextConnector.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibary.Models;
+using TrackerLibary.DataAccess.TextHelpers;
 
 namespace TrackerLibary.DataAccess
 {
@@ -10,9 +11,13 @@ namespace TrackerLibary.DataAccess
 		// TODO - Wire up the CreatePrize for text files.
 		public PrizeModel CreatePrize(PrizeModel model)
 		{
-			model.Id = 1;
+			// Load the text file
+			// Convert the text to List<PrizeModel>
+			// Find the ID
+			// Add the new record with the new ID
+			// Convert the prizes to List<string>
+			// Save the List<string> to the text file
 
-			return model;
 		}
 	}
 }
